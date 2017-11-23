8e9722cd8c640f318641918a129a95a2d9f35985
zsrzsrzsr-zsr <691363707@qq.com>
2017-11-23 13:52:50 +0800

    first updata

--- js/details.js
+++ js/details.js
@@ -22,43 +22,14 @@ $('.footer ul .phone').tap(function(){
 $('.footer ul .join').tap(function(){
 	window.location.href="apply.html"
 })
-
-$.fn.scrollTo =function(options){
-  var defaults = {
-      toT : 0,    //滚动目标位置
-      durTime : 500,  //过渡动画时间
-      delay : 30,     //定时器时间
-      callback:null   //回调函数
-  };
-  var opts = $.extend(defaults,options),
-      timer = null,
-      _this = this,
-      curTop = _this.scrollTop(),//滚动条当前的位置
-      subTop = opts.toT - curTop,    //滚动条目标位置和当前位置的差值
-      index = 0,
-      dur = Math.round(opts.durTime / opts.delay),
-      smoothScroll = function(t){
-          index++;
-          var per = Math.round(subTop/dur);
-          if(index >= dur){
-              _this.scrollTop(t);
-              window.clearInterval(timer);
-              if(opts.callback && typeof opts.callback == 'function'){
-                  opts.callback();
-              }
-              return;
-          }else{
-              _this.scrollTop(curTop + index*per);
-          }
-      };
-  timer = window.setInterval(function(){
-      smoothScroll(opts.toT);
-  }, opts.delay);
-  return _this;
-};
+var top1 = document.querySelector('.intro').offsetTop 
+var top2 = document.querySelector('.notice').offsetTop 
+console.log(top1)
 $('.feature .list ul li').eq(1).tap(function(){
-	$('body,html').scrollTo({toT:1910})
+  console.log(1)
+  $('body,html').scrollTop(top1)
 })
 $('.feature .list ul li').eq(2).tap(function(){
-	$('body,html').scrollTo({toT:5180})
+  console.log(1)
+  $('body,html').scrollTop(top2)
 })
--- js/index.js
+++ js/index.js
@@ -80,48 +80,14 @@ function fun(){
 }
 	setTimeout(fun,1000)
 
-$.fn.scrollTo =function(options){
-  var defaults = {
-      toT : 0,    //滚动目标位置
-      durTime : 500,  //过渡动画时间
-      delay : 30,     //定时器时间
-      callback:null   //回调函数
-  };
-  var opts = $.extend(defaults,options),
-      timer = null,
-      _this = this,
-      curTop = _this.scrollTop(),//滚动条当前的位置
-      subTop = opts.toT - curTop,    //滚动条目标位置和当前位置的差值
-      index = 0,
-      dur = Math.round(opts.durTime / opts.delay),
-      smoothScroll = function(t){
-          index++;
-          var per = Math.round(subTop/dur);
-          if(index >= dur){
-              _this.scrollTop(t);
-              window.clearInterval(timer);
-              if(opts.callback && typeof opts.callback == 'function'){
-                  opts.callback();
-              }
-              return;
-          }else{
-              _this.scrollTop(curTop + index*per);
-          }
-      };
-  timer = window.setInterval(function(){
-      smoothScroll(opts.toT);
-  }, opts.delay);
-  return _this;
-};
-$('.area .allarea').tap(function(){
-	$('body,html').scrollTo({toT:530})
+var top1 = document.querySelector('.aozhou').offsetTop
+$('.area>a').eq(2).tap(function(){
+  $('body,html').scrollTop(top1)
 })
 $('.area .cisborder').tap(function(){
 	$(this).attr('href','area.html')
 })
-$('.area .overseas').tap(function(){
-	$('body,html').scrollTo({toT:950})
-})
+// 
 $('.load').tap(function(){
 	if($(this).css('background-color')=='rgb(241, 241, 241)'){
 		$(this).css('background-color','#50BBDB')
